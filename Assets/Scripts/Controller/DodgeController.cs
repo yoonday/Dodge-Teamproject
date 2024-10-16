2a5f82873a54b55d99a64dc0fd5a2bf8b6952644
udiladido <89720389+udiladido@users.noreply.github.com>
2024-10-16 21:18:27 +0900

    Revert "Merge branch 'dev' into ChoiEojin"
    This reverts commit 8386e4bb22a660b87f123c8b755bac56161fe263, reversing
    changes made to 596cb1d5d3ae6d5ee31688a39fc454cd2310fa41.

--- Assets/Scripts/Controller/DodgeController.cs
+++ Assets/Scripts/Controller/DodgeController.cs
@@ -9,18 +9,17 @@ public class DodgeController : MonoBehaviour
     public event Action OnAttackEvent;
 
     private float timeSinceLastAttack = float.MaxValue;
-    protected bool isAttacking { get; set; }
-    protected PlayerStatHandler stats {  get; set; }
+    protected bool isAttacking;
 
     protected virtual void Awake()
     {
-        stats = GetComponent<PlayerStatHandler>();
+
     }
 
     // Update is called once per frame
     protected virtual void Update()
     {
-        HandleAttackDelay();
+        
     }
 
     public void CallMoveEvent(Vector2 direction)
@@ -31,18 +30,5 @@ public class DodgeController : MonoBehaviour
     public void CallAttackEvent()
     {
         OnAttackEvent?.Invoke();
-    }
-
-    private void HandleAttackDelay()
-    {
-        if (timeSinceLastAttack < stats.CurrentStat.playerSO.delay)
-        {
-            timeSinceLastAttack += Time.deltaTime;
-        }
-        else if (isAttacking && timeSinceLastAttack >= stats.CurrentStat.playerSO.delay)
-        {
-            timeSinceLastAttack = 0;
-            CallAttackEvent(); 
-        }
     }
 }
