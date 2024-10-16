b00d54149370e23c8adfd039d5de3551bc6bc47c
udiladido <89720389+udiladido@users.noreply.github.com>
2024-10-16 15:58:11 +0900

    feat : UI Controller 기능 추가
    플레이어 상태 변화에 따른 UI 변화

--- Assets/Scripts/Behaviors/HealthSystem.cs
+++ Assets/Scripts/Behaviors/HealthSystem.cs
@@ -20,7 +20,6 @@ public class HealthSystem : MonoBehaviour
     {
 
 
-
         if (CurrentHealth <= 0f)
         {
             CallDeath();
--- Assets/Scripts/Controller/DodgeUIController.cs
+++ Assets/Scripts/Controller/DodgeUIController.cs
@@ -1,11 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class DodgeUIController : MonoBehaviour
 {
-    [SerializeField] private 
+    //1P, 2P 플레이어 이름 가져오기, 
 
+    [SerializeField] private TextMeshProUGUI playerName;
+    [SerializeField] private Image playerSkillimage;
+    [SerializeField] private Image[] playerHealth;
+    [SerializeField] private TextMeshProUGUI score;
+
+    private HealthSystem healthSystem;
+
+
+    private int currentScore = 0;
+
+    private void Awake()
+    {
+
+        playerName = GameObject.Find($"{gameObject.name}UI").GetComponent<TextMeshProUGUI>();
+        playerSkillimage = GameObject.Find($"{gameObject.name}SkillIcon").GetComponent<Image>();
+        playerHealth = GameObject.Find($"{gameObject.name}Health").GetComponentsInChildren<Image>();
+        score = GameObject.Find("GameScore").GetComponent<TextMeshProUGUI>();
+
+
+        healthSystem = GetComponent<HealthSystem>();
+    }
+
+
+    private void Start()
+    {
+
+        healthSystem.OnDamage += PlayerLostHealth;
+        healthSystem.OnHeal += PlayerGetHealth;
+
+    }
+
+    private void PlayerGetHealth()
+    {
+      
+
+    }
+
+    private void PlayerLostHealth()
+    {
+
+        //플레이어 health 이미지 비활성화 
+
+
+
+    }
 
 
 
