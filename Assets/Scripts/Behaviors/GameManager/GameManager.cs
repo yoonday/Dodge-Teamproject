c2771cc6d1b2df4b118bb10db6568860571d25de
TaekyoungLee02 <102038651+TaekyoungLee02@users.noreply.github.com>
2024-10-17 11:47:51 +0900

    Reapply "Merge branch 'dev2' into altkzkss"
    This reverts commit 92c76199751a6c7b04b3fa78ee87a194ab94d9e1.

---
+++ Assets/Scripts/Behaviors/GameManager/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    public PoolManager ObjectPool { get; private set; }
+
+
+
+
+}
