9dea58b825ee024a284659aa416529c5ca20b22a
TaekyoungLee02 <102038651+TaekyoungLee02@users.noreply.github.com>
2024-10-17 19:56:07 +0900

    Update DestroyOnDeath.cs

--- Assets/Scripts/Behaviors/DestroyOnDeath.cs
+++ Assets/Scripts/Behaviors/DestroyOnDeath.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class DestroyOnDeath : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private HealthSystem healthSystem;
+
+    private void Start()
     {
-        
+        healthSystem = GetComponent<HealthSystem>();
+        healthSystem.OnDeath += OnDeath;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnDeath()
     {
-        
+        /*
+         *      죽었을 때 아이템 생성되게.
+         *      적이 죽을 때 이펙트 반영?
+         *      
+         *      
+         *      아이템이 만들어지는 경우의 수
+         *      적이 죽었을 때
+         *      
+         *      ItemSpawner -> 아이템 생성 (랜덤)
+         *      
+         *      transform.position
+         */
+
+
+        Destroy(gameObject);
     }
 }
