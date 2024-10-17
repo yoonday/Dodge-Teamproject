e04efd0760cb5c62e8a79045a2ea8ff4a2ca26df
yoonday <yoon2dev@gmail.com>
2024-10-17 20:03:42 +0900

    feat: Add collision handling script
    충돌 처리를 위한 새로운 C# 스크립트를 추가

--- Assets/Scripts/Behaviors/HealthSystem.cs
+++ Assets/Scripts/Behaviors/HealthSystem.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
+    [SerializeField] private PlayerStatHandler playerStatHandler; // 스탯을 HeathSystem에서 관리하는 것이 용이할 듯
 
-    
 
     public event Action OnDamage;
     public event Action OnHeal;
@@ -15,11 +15,24 @@ public class HealthSystem : MonoBehaviour
      
     private float timeSinceLastChange = float.MaxValue;
     public float CurrentHealth { get; private set; }
-    private bool isAttacked = false;
+    private bool isAttacked = false; // 어디서 사용하는지?
 
+    private void Start()
+    {
+
+        if (playerStatHandler != null) // 초기 스탯 설정
+        {
+            CurrentHealth = playerStatHandler.CurrentStat.maxHealth;
+        }
+        else
+        {
+            Debug.LogWarning("PlayerStatHandler 연결 필요");
+        }
+    }
 
     public bool ChangeHealth(int change)
     {
+        CurrentHealth += change; // 체력에 값 변화
 
         if (CurrentHealth <= 0f)
         {
@@ -27,6 +40,7 @@ public class HealthSystem : MonoBehaviour
             return true;
 
         }
+
 
         if (change >= 0)
         {
@@ -47,7 +61,6 @@ public class HealthSystem : MonoBehaviour
     {
 
         OnDeath?.Invoke();
-
 
     }
 
