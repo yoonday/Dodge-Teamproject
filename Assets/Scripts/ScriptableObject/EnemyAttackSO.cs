9be54e6326fc5b3612b0e514140c85c196f82405
TaekyoungLee02 <102038651+TaekyoungLee02@users.noreply.github.com>
2024-10-18 19:33:14 +0900

    Update EnemyAttackSO.cs

--- Assets/Scripts/ScriptableObject/EnemyAttackSO.cs
+++ Assets/Scripts/ScriptableObject/EnemyAttackSO.cs
@@ -3,20 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "EnemyAttackSO", menuName = "TopDownController/Attacks/Enemy", order = 0)]
-public class EnemyAttackSO : ScriptableObject
+public class EnemyAttackSO : PlayerAttackSO
 {
-    [Header("Base Attack Info")]
-    public float size;
-    public float delay;
-    public float power;
-    public float speed;
-    public LayerMask target;
-
     [Header("Attack Type Info (Amount Should be Bigger than 2)")]
     public Angle angle;
     public float angleDeg;
     public int amount;
     public int attackCountAtOnce;
+
+    [Header("Enemy Stat")]
+    public int enemyHealth;
 
     public enum Angle
     {
