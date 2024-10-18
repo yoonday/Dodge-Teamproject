8fbfd7a123ffe6bbb681db65cf0e2efd71d400bd
udiladido <89720389+udiladido@users.noreply.github.com>
2024-10-18 15:18:01 +0900

    feat : 플레이어 생성
    씬 전환에 따른 플레이어 위치 및 프리팹 설정

--- Assets/Scripts/Controller/PlayerController.cs
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -1,24 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
+
+    //추후 GameManager로 옮기기 - SpawnPlayer();
+
+    [SerializeField] private Vector3[] playerSpawnPoint;
+    [SerializeField] private int playerNum;
+    [SerializeField] private GameObject[] playerPrefab;
+
+
+    private void OnSceneLoaed(Scene scene, LoadSceneMode mode)
+    {
+
+        if (scene.name == "씬 번호로 받기")
+        {
+
+            InitializeGame();
+
+        }
+
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaed;
+
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaed;
+
+    }
+
+
+    private void InitializeGame()
+    {
+
+
+        SetSpanwPoint();
+        SpawnPlayer();
+
+    }
     
+
+    void SetSpanwPoint()
+    {
+
+        playerSpawnPoint = new Vector3[]{
+
+            new Vector3(0,-14,0),
+            new Vector3(-5,-14,0),
+            new Vector3(5,-14,0)
+
+        };
+
+    }
+
+    void SpawnPlayer()
+    {
+
+        for (int i = 0; i < playerNum; i++)
+        {
+
+            if (playerNum == 1)
+                Instantiate(playerPrefab[i], playerSpawnPoint[2], Quaternion.identity);
+
+            else
+                Instantiate(playerPrefab[i], playerSpawnPoint[i], Quaternion.identity);
+        }
    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-       
-
-
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
 }
