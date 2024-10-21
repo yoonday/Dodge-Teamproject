a09b1adb6d78067d656ba849eb9244a4c9f226f5
TaekyoungLee02 <102038651+TaekyoungLee02@users.noreply.github.com>
2024-10-21 10:31:19 +0900

    rename : 함수로 정리

--- Assets/Scripts/Controller/DodgeEnemyController.cs
+++ Assets/Scripts/Controller/DodgeEnemyController.cs
@@ -40,17 +40,7 @@ public class DodgeEnemyController : DodgeController
     // Start is called before the first frame update
     void Start()
     {
-        Camera mainCamera = Camera.main;
-        enemySprite = transform.GetComponentInChildren<SpriteRenderer>().sprite;
-
-        float cameraHeight = mainCamera.orthographicSize;
-        float cameraWidth = cameraHeight * ((float)Screen.width / Screen.height);
-
-        randomPosRangeYMin = mainCamera.transform.position.y + cameraHeight - (enemySprite.bounds.size.y / 2); // 화면 맨 위 좌표.
-        randomPosRangeYMax = randomPosRangeYMin - cameraHeight; // 적이 나타날 화면 맨 아래 좌표.
-
-        randomPosRangeXMin = mainCamera.transform.position.x - cameraWidth + (enemySprite.bounds.size.x / 2); // 화면 맨 왼쪽 좌표.
-        randomPosRangeXMax = mainCamera.transform.position.x + cameraWidth - (enemySprite.bounds.size.x / 2); // 화면 맨 오른쪽 좌표.
+        SetRange();
     }
 
     protected override void Update() { }
@@ -79,5 +69,20 @@ public class DodgeEnemyController : DodgeController
             }
             else yield return null;
         }
+    }
+
+    private void SetRange()
+    {
+        Camera mainCamera = Camera.main;
+        enemySprite = transform.GetComponentInChildren<SpriteRenderer>().sprite;
+
+        float cameraHeight = mainCamera.orthographicSize;
+        float cameraWidth = cameraHeight * ((float)Screen.width / Screen.height);
+
+        randomPosRangeYMin = mainCamera.transform.position.y + cameraHeight - (enemySprite.bounds.size.y / 2); // 화면 맨 위 좌표.
+        randomPosRangeYMax = randomPosRangeYMin - cameraHeight; // 적이 나타날 화면 맨 아래 좌표.
+
+        randomPosRangeXMin = mainCamera.transform.position.x - cameraWidth + (enemySprite.bounds.size.x / 2); // 화면 맨 왼쪽 좌표.
+        randomPosRangeXMax = mainCamera.transform.position.x + cameraWidth - (enemySprite.bounds.size.x / 2); // 화면 맨 오른쪽 좌표.
     }
 }
