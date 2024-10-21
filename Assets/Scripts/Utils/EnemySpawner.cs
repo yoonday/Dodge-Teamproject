7dfe865f4382170e79544ddd96427c2fc3ae2da4
TaekyoungLee02 <102038651+TaekyoungLee02@users.noreply.github.com>
2024-10-21 13:00:27 +0900

    feat : turn enemyspawner to singleton

--- Assets/Scripts/Utils/EnemySpawner.cs
+++ Assets/Scripts/Utils/EnemySpawner.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
-public class EnemySpawner : MonoBehaviour
+public class EnemySpawner : Singleton<EnemySpawner>
 {
     [SerializeField] GameObject enemyPrefab;
     [SerializeField] EnemyInfo[] enemyInfos;
     [SerializeField] float spawnCooltime;
-    [SerializeField] float levelUpTime;
+    [SerializeField] int eliteMobFreq;
+    [SerializeField] int bossMobFreq;
 
-    private float gameTime;
     private int currentLevel;
+    private int destroyedEnemyCount;
 
     private float randomPosRangeXMin;
     private float randomPosRangeXMax;
@@ -23,24 +25,19 @@ public class EnemySpawner : MonoBehaviour
 
     void Start()
     {
-        Camera mainCamera = Camera.main;
-
-        float cameraHeight = mainCamera.orthographicSize;
-        float cameraWidth = cameraHeight * ((float)Screen.width / Screen.height);
-
-        posY = mainCamera.transform.position.y + cameraHeight * 2; // 화면 맨 위 좌표.
-
-        randomPosRangeXMin = mainCamera.transform.position.x - cameraWidth; // 화면 맨 왼쪽 좌표.
-        randomPosRangeXMax = mainCamera.transform.position.x + cameraWidth; // 화면 맨 오른쪽 좌표.
+        destroyedEnemyCount = 0;
+        SetRange();
 
         StartCoroutine(SpawnEnemy());
+
+        SceneManager.sceneLoaded += DestroyEnemySpawner;
     }
 
     private IEnumerator SpawnEnemy()
     {
         while (true)
         {
-            currentLevel = (int)(gameTime / levelUpTime) + 1;
+            currentLevel = 1;
 
             var tempEnemies = from info in enemyInfos
                                  where info.level <= currentLevel
@@ -56,6 +53,33 @@ public class EnemySpawner : MonoBehaviour
             enemy.GetComponent<DodgeEnemyController>().Init(1, currentEnemies[rand]);
 
             yield return new WaitForSeconds(spawnCooltime);
+        }
+    }
+
+    public void EnemyDestroyed()
+    {
+        destroyedEnemyCount++;
+    }
+
+    private void SetRange()
+    {
+        Camera mainCamera = Camera.main;
+
+        float cameraHeight = mainCamera.orthographicSize;
+        float cameraWidth = cameraHeight * ((float)Screen.width / Screen.height);
+
+        posY = mainCamera.transform.position.y + cameraHeight * 2; // 화면 맨 위 좌표.
+
+        randomPosRangeXMin = mainCamera.transform.position.x - cameraWidth; // 화면 맨 왼쪽 좌표.
+        randomPosRangeXMax = mainCamera.transform.position.x + cameraWidth; // 화면 맨 오른쪽 좌표.
+    }
+
+    private void DestroyEnemySpawner(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Dodge_Project_Start")
+        {
+            SceneManager.sceneLoaded -= DestroyEnemySpawner;
+            Destroy(gameObject);
         }
     }
 }
